3ee50aecf451128952e0b711457e70a3342e2f16
Josh Baird <jjbaird@oakland.edu>
2017-01-21 15:02:32 -0500

    Implement some of OUPeople

--- src/edu/oakland/OUSoft/database/OUPeople.java
+++ src/edu/oakland/OUSoft/database/OUPeople.java
@@ -1,5 +1,11 @@
 package edu.oakland.OUSoft.database;
 
+import edu.oakland.OUSoft.items.Instructor;
+import edu.oakland.OUSoft.items.Person;
+import edu.oakland.OUSoft.items.Student;
+
+import java.util.ArrayList;
+
 /**
  * includes an array of references to all Students and an array of references to all Instructors,
  * methods to add students and instructors,
@@ -9,4 +15,54 @@ package edu.oakland.OUSoft.database;
  * methods for taking input from command line
  */
 public class OUPeople {
+	
+	private ArrayList students;
+	private ArrayList instructors;
+	private ArrayList others;
+	
+	/**
+	 * Retrieve a student using their ID
+	 *
+	 * @param ID The students ID
+	 * @return The student, or null if not found
+	 */
+	public Student retrieveStudentByID(int ID) {
+		for (Student student : students) {
+			if (student.getID() == ID) {
+				return student;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Retrieve an instructor using their ID
+	 *
+	 * @param ID The instructors ID
+	 * @return The instructor, or null if not found
+	 */
+	public Student retrieveInstructorByID(int ID) {
+		for (Instructor instructor : instructors) {
+			if (instructor.getID() == ID) {
+				return instructor;
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 * Add a person to the database, automatically determining type
+	 *
+	 * @param person The Person to add
+	 */
+	public void add(Person person) {
+		if (person instanceof Student) {
+			this.students.add((Student) person);
+		} else if (person instanceof Instructor) {
+			this.instructors.add((Instructor) person);
+		} else {
+			this.others.add(person);
+		}
+		
+	}
 }
