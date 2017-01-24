74b9cd392009d75c2b9679e4135000b6d50c2b22
Josh Baird <jjbaird@oakland.edu>
2017-01-24 00:08:37 -0500

    Update main class to start text interface

--- src/edu/oakland/OUSoft/Main.java
+++ src/edu/oakland/OUSoft/Main.java
@@ -7,5 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		OUPeople people = new OUPeople();
 		
+		TextInterface ti = new TextInterface(people);
+		
+		ti.startup();
+		
 	}
 }
