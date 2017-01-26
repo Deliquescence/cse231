2a311b51ee5211ae28f6d3275e5929035e645daa
Josh Baird <jjbaird@oakland.edu>
2017-01-26 17:26:17 -0500

    Add tests

--- test/edu/oakland/OUSoft/database/OUPeopleTest.java
+++ test/edu/oakland/OUSoft/database/OUPeopleTest.java
@@ -1,22 +1,175 @@
 package edu.oakland.OUSoft.database;
 
+import edu.oakland.OUSoft.items.Course;
 import edu.oakland.OUSoft.items.Instructor;
 import edu.oakland.OUSoft.items.Person;
 import edu.oakland.OUSoft.items.Student;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class OUPeopleTest {
-	
 	private OUPeople db;
 	
 	@BeforeEach
-	void setUp(){
+	void setUp() {
 		db = new OUPeople();
+	}
+	
+	@Test
+	void addCourse() {
+		Course testCourse = new Course("testCourse");
+		Instructor testInstructor = new Instructor("TestInstructorID");
+		testCourse.setInstructor(testInstructor);
+		
+		db.addCourse(testCourse);
+		assertTrue(db.getCourses().contains(testCourse), "addCourse did not add a course");
+	}
+	
+	@Test
+	void addCourseOverload() {
+		Course testCourse = new Course("testCourse");
+		Instructor testInstructor = new Instructor("TestInstructorID");
+		
+		db.addCourse(testCourse, testInstructor);
+		assertTrue(db.getCourses().contains(testCourse), "addCourse did not add a course when passed a course and instructor");
+	}
+	
+	@Test
+	void addCourseNoInstructor() {
+		Course testCourse = new Course("testCourse");
+		
+		//Must have an instructor
+		boolean threwExceptionForNoInstructor = false;
+		try {
+			db.addCourse(testCourse);
+		} catch (IllegalArgumentException shouldThrow) {
+			threwExceptionForNoInstructor = true;
+		}
+		assertTrue(threwExceptionForNoInstructor, "addCourse did not throw an exception for a course not having a valid Instructor");
+	}
+	
+	@Test
+	void enroll() {
+		Course testCourse = new Course("testCourse");
+		Student testStudent = new Student("TestStudentID");
+		
+		Enrollment testEnrollment = new Enrollment(testCourse, testStudent);
+		Enrollment returnedEnrollment = db.enroll(testStudent, testCourse);
+		
+		assertNotNull(returnedEnrollment, "enroll returned null");
+		assertEquals(testEnrollment, returnedEnrollment, "enroll return did not equal the expected Enrollment");
+	}
+	
+	@Test
+	void withdraw() {
+		Course testCourse = new Course("testCourse");
+		Student testStudent = new Student("TestStudentID");
+		
+		Enrollment enrollment = db.enroll(testStudent, testCourse);
+		assertNotNull(db.getEnrollment(testStudent, testCourse), "Enroll didn't work");
+		
+		db.withdraw(testStudent, testCourse);
+		assertNull(db.getEnrollment(testStudent, testCourse), "withdraw didn't withdraw");
+	}
+	
+	@Test
+	void getEnrollment() {
+		Course testCourse = new Course("testCourse");
+		Student testStudent = new Student("TestStudentID");
+		
+		assertNull(db.getEnrollment(testStudent, testCourse), "Magically got enrolled somehow");
+		
+		Enrollment returnedEnrollment = db.enroll(testStudent, testCourse);
+		
+		assertEquals(returnedEnrollment, db.getEnrollment(testStudent, testCourse), "getEnrollment did not return the same as enroll()");
+	}
+	
+	@Test
+	void getEnrollmentsForCourse() {
+		Course testCourse = new Course("testCourse");
+		Student testStudent = new Student("TestStudentID");
+		Student testStudent2 = new Student("TestStudentID2");
+		
+		assertEquals(0, db.getEnrollments(testCourse).size(), "getEnrollments (for course) magically obtained an Enrollment");
+		
+		Enrollment returnedEnrollment = db.enroll(testStudent, testCourse);
+		Enrollment returnedEnrollment2 = db.enroll(testStudent2, testCourse);
+		
+		List<Enrollment> enrollments = db.getEnrollments(testCourse);
+		
+		assertTrue(enrollments.contains(returnedEnrollment), "getEnrollments (for course) did not contain an Enrollment it should have");
+		assertTrue(enrollments.contains(returnedEnrollment2), "getEnrollments (for course) did not contain an Enrollment it should have");
+	}
+	
+	@Test
+	void getEnrollmentsForStudent() {
+		Course testCourse = new Course("testCourse");
+		Course testCourse2 = new Course("testCourse2");
+		Student testStudent = new Student("TestStudentID");
+		
+		assertEquals(0, db.getEnrollments(testStudent).size(), "getEnrollments (for student) magically obtained an Enrollment");
+		
+		Enrollment returnedEnrollment = db.enroll(testStudent, testCourse);
+		Enrollment returnedEnrollment2 = db.enroll(testStudent, testCourse2);
+		
+		List<Enrollment> enrollments = db.getEnrollments(testStudent);
+		
+		assertTrue(enrollments.contains(returnedEnrollment), "getEnrollments (for student) did not contain an Enrollment it should have");
+		assertTrue(enrollments.contains(returnedEnrollment2), "getEnrollments (for student) did not contain an Enrollment it should have");
+	}
+	
+	@Test
+	void studentIsEnrolled() {
+		Course testCourse = new Course("testCourse");
+		Student testStudent = new Student("TestStudentID");
+		
+		assertFalse(db.studentIsEnrolled(testStudent, testCourse), "Student magically got enrolled");
+		
+		db.enroll(testStudent, testCourse);
+		
+		assertTrue(db.studentIsEnrolled(testStudent, testCourse), "Student did not get enrolled");
+	}
+	
+	@Test
+	void numberStudentsEnrolled() {
+		Course testCourse = new Course("testCourse");
+		Student testStudent = new Student("TestStudentID");
+		Student testStudent2 = new Student("TestStudentID2");
+		
+		assertEquals(0, db.numberStudentsEnrolled(testCourse), "Should be no one enrolled");
+		db.enroll(testStudent, testCourse);
+		assertEquals(1, db.numberStudentsEnrolled(testCourse), "Should be one person enrolled");
+		db.enroll(testStudent2, testCourse);
+		assertEquals(2, db.numberStudentsEnrolled(testCourse), "Should be two people enrolled");
+		db.withdraw(testStudent, testCourse);
+		assertEquals(1, db.numberStudentsEnrolled(testCourse), "Should be one person enrolled");
+		
+		//Call enroll again when they are already enrolled that course
+		db.enroll(testStudent2, testCourse);
+		assertEquals(1, db.numberStudentsEnrolled(testCourse), "numberStudentsEnrolled says more students got enrolled after the same person was enrolled again");
+	}
+	
+	@Test
+	void numberCoursesEnrolled() {
+		Course testCourse = new Course("testCourse");
+		Course testCourse2 = new Course("testCourse2");
+		Student testStudent = new Student("TestStudentID");
+		
+		assertEquals(0, db.numberCoursesEnrolled(testStudent), "Should be no courses enrolled");
+		db.enroll(testStudent, testCourse);
+		assertEquals(1, db.numberCoursesEnrolled(testStudent), "Should be one course enrolled");
+		db.enroll(testStudent, testCourse2);
+		assertEquals(2, db.numberCoursesEnrolled(testStudent), "Should be two courses enrolled");
+		db.withdraw(testStudent, testCourse);
+		assertEquals(1, db.numberCoursesEnrolled(testStudent), "Should be one course enrolled");
+		
+		//Call enroll again when they are already enrolled that course
+		db.enroll(testStudent, testCourse2);
+		assertEquals(1, db.numberCoursesEnrolled(testStudent), "numberCoursesEnrolled says more courses got enrolled after the same course was enrolled again");
 	}
 	
 	@Test
