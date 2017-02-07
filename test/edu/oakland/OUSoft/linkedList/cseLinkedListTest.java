38a25a793a4760232eeee8679876b7abea8ca723
Josh Baird <jjbaird@oakland.edu>
2017-02-07 10:39:14 -0500

    Instead of enrollments, use people in list test
    To better check type issues

--- test/edu/oakland/OUSoft/linkedList/cseLinkedListTest.java
+++ test/edu/oakland/OUSoft/linkedList/cseLinkedListTest.java
@@ -1,7 +1,7 @@
 package edu.oakland.OUSoft.linkedList;
 
-import edu.oakland.OUSoft.database.Enrollment;
-import edu.oakland.OUSoft.items.Course;
+import edu.oakland.OUSoft.items.Instructor;
+import edu.oakland.OUSoft.items.Person;
 import edu.oakland.OUSoft.items.Student;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,13 +15,19 @@ import static org.junit.Assert.*;
 
 public class cseLinkedListTest {
 	
-	private cseLinkedList<Enrollment> list;
-//	private ArrayList<Enrollment> list;
+	private cseLinkedList<Person> list;
+//	private ArrayList<Person> list;
 	
-	private Enrollment testEnrollment1 = new Enrollment(new Course("TestCourse"), new Student("TestID"));
-	private Enrollment testEnrollment2 = new Enrollment(new Course("TestCourse2"), new Student("TestID2"));
-	private Enrollment testEnrollment3 = new Enrollment(new Course("TestCourse3"), new Student("TestID3"));
-	private Enrollment testEnrollment4 = new Enrollment(new Course("TestCourse4"), new Student("TestID4"));
+	private Person testPerson1 = new Person("TestID1");
+	private Person testPerson2 = new Person("TestID2");
+	private Person testPerson3 = new Person("TestID3");
+	private Person testPerson4 = new Person("TestID4");
+	
+	private Student testStudent1 = new Student("TestStudentID1");
+	private Student testStudent2 = new Student("TestStudentID2");
+	
+	private Instructor testInstructor1 = new Instructor("TestInstructorID1");
+	private Instructor testInstructor2 = new Instructor("TestInstructorID2");
 	
 	@Before
 	public void setUp() throws Exception {
@@ -32,91 +38,91 @@ public class cseLinkedListTest {
 	@Test
 	public void size() throws Exception {
 		assertEquals(0, list.size());
-		list.add(testEnrollment1);
+		list.add(testPerson1);
 		assertEquals(1, list.size());
-		list.add(testEnrollment2);
+		list.add(testPerson2);
 		assertEquals(2, list.size());
-		list.remove(testEnrollment2);
+		list.remove(testPerson2);
 		assertEquals(1, list.size());
 	}
 	
 	@Test
 	public void isEmpty() throws Exception {
 		assertTrue(list.isEmpty());
-		list.add(testEnrollment1);
+		list.add(testPerson1);
 		assertFalse(list.isEmpty());
-		list.remove(testEnrollment1);
+		list.remove(testPerson1);
 		assertTrue(list.isEmpty());
 	}
 	
 	@Test
 	public void contains() throws Exception {
-		assertFalse(list.contains(testEnrollment1));
-		list.add(testEnrollment1);
-		assertTrue(list.contains(testEnrollment1));
+		assertFalse(list.contains(testPerson1));
+		list.add(testPerson1);
+		assertTrue(list.contains(testPerson1));
 		assertFalse(list.contains("Some object"));
 		assertFalse(list.contains(null));
 	}
 	
 	@Test
 	public void iterator() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
-		Iterator<Enrollment> iterator = list.iterator();
+		list.add(testPerson1);
+		list.add(testPerson2);
+		Iterator<Person> iterator = list.iterator();
 		
 		assertTrue(iterator.hasNext());
-		assertEquals("Iterator didn't give expected first element", testEnrollment1, iterator.next());
+		assertEquals("Iterator didn't give expected first element", testPerson1, iterator.next());
 		assertTrue(iterator.hasNext());
-		assertEquals("Iterator didn't give expected second element", testEnrollment2, iterator.next());
+		assertEquals("Iterator didn't give expected second element", testPerson2, iterator.next());
 		assertFalse(iterator.hasNext());
 	}
 	
 	@Test
 	public void toArray() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
+		list.add(testPerson1);
+		list.add(testPerson2);
 		
 		Object[] array = list.toArray();
-		assertEquals(testEnrollment1, array[0]);
-		assertEquals(testEnrollment2, array[1]);
+		assertEquals(testPerson1, array[0]);
+		assertEquals(testPerson2, array[1]);
 	}
 	
 	@Test
 	public void toArray1() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
+		list.add(testPerson1);
+		list.add(testPerson2);
 		
-		Enrollment[] array = list.toArray(new Enrollment[0]);
-		assertEquals(testEnrollment1, array[0]);
-		assertEquals(testEnrollment2, array[1]);
+		Person[] array = list.toArray(new Person[0]);
+		assertEquals(testPerson1, array[0]);
+		assertEquals(testPerson2, array[1]);
 		
-		Enrollment[] array2 = new Enrollment[2];
+		Person[] array2 = new Person[2];
 		list.toArray(array2);
-		assertEquals(testEnrollment1, array2[0]);
-		assertEquals(testEnrollment2, array2[1]);
+		assertEquals(testPerson1, array2[0]);
+		assertEquals(testPerson2, array2[1]);
 		
-		Enrollment[] array3 = new Enrollment[3];
-		array3[2] = testEnrollment4;
+		Person[] array3 = new Person[3];
+		array3[2] = testPerson4;
 		list.toArray(array3);
-		assertEquals(testEnrollment1, array3[0]);
-		assertEquals(testEnrollment2, array3[1]);
+		assertEquals(testPerson1, array3[0]);
+		assertEquals(testPerson2, array3[1]);
 		assertNull(array3[2]);
 	}
 	
 	@Test
 	public void add() throws Exception {
-		assertTrue(list.add(testEnrollment1));
-		assertTrue(list.contains(testEnrollment1));
-		assertEquals(testEnrollment1, list.get(0));
+		assertTrue(list.add(testPerson1));
+		assertTrue(list.contains(testPerson1));
+		assertEquals(testPerson1, list.get(0));
 		assertEquals(1, list.size());
-		assertTrue(list.add(testEnrollment2));
-		assertTrue(list.contains(testEnrollment2));
-		assertEquals(testEnrollment2, list.get(1));
+		assertTrue(list.add(testPerson2));
+		assertTrue(list.contains(testPerson2));
+		assertEquals(testPerson2, list.get(1));
 		assertEquals(2, list.size());
 		
 		//Do not allow duplicate
 		try {
-			list.add(testEnrollment1);
+			list.add(testPerson1);
 			assertTrue("Added a duplicate element", false);
 			
 		} catch (IllegalArgumentException ex) {
@@ -133,20 +139,20 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void remove() throws Exception {
-		assertFalse(list.remove(testEnrollment1));
-		list.add(testEnrollment1);
-		assertTrue(list.remove(testEnrollment1));
-		assertFalse(list.contains(testEnrollment1));
+		assertFalse(list.remove(testPerson1));
+		list.add(testPerson1);
+		assertTrue(list.remove(testPerson1));
+		assertFalse(list.contains(testPerson1));
 		
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
-		list.add(testEnrollment3);
+		list.add(testPerson1);
+		list.add(testPerson2);
+		list.add(testPerson3);
 		assertEquals(3, list.size());
 		
-		assertTrue(list.remove(testEnrollment2));
-		assertEquals(testEnrollment1, list.get(0));
-		assertEquals(testEnrollment3, list.get(1));
-		assertFalse(list.contains(testEnrollment2));
+		assertTrue(list.remove(testPerson2));
+		assertEquals(testPerson1, list.get(0));
+		assertEquals(testPerson3, list.get(1));
+		assertFalse(list.contains(testPerson2));
 		
 		try {
 			assertFalse("Should have returned false for null remove", list.remove(null));
@@ -156,26 +162,26 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void containsAll() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
-		list.add(testEnrollment3);
+		list.add(testPerson1);
+		list.add(testPerson2);
+		list.add(testPerson3);
 		assertEquals(3, list.size());
 		
-		Collection<Enrollment> testCollection1 = new ArrayList<>();
-		Collection<Enrollment> testCollection2 = new ArrayList<>();
-		Collection<Enrollment> testCollection3 = new ArrayList<>();
+		Collection<Person> testCollection1 = new ArrayList<>();
+		Collection<Person> testCollection2 = new ArrayList<>();
+		Collection<Person> testCollection3 = new ArrayList<>();
 		
-		testCollection1.add(testEnrollment2);
+		testCollection1.add(testPerson2);
 		
-		testCollection2.add(testEnrollment1);
-		testCollection2.add(testEnrollment2);
-		testCollection2.add(testEnrollment3);
+		testCollection2.add(testPerson1);
+		testCollection2.add(testPerson2);
+		testCollection2.add(testPerson3);
 		
 		assertTrue(list.containsAll(testCollection1));
 		assertTrue(list.containsAll(testCollection2));
 		assertTrue(list.containsAll(testCollection3));
 		
-		list.remove(testEnrollment3);
+		list.remove(testPerson3);
 		assertFalse(list.containsAll(testCollection2));
 		
 		List<String> wrongType = new ArrayList<>();
@@ -198,17 +204,17 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void addAll() throws Exception {
-		Collection<Enrollment> testCollection1 = new ArrayList<>();
+		Collection<Person> testCollection1 = new ArrayList<>();
 		
-		testCollection1.add(testEnrollment1);
-		testCollection1.add(testEnrollment2);
-		testCollection1.add(testEnrollment3);
+		testCollection1.add(testPerson1);
+		testCollection1.add(testPerson2);
+		testCollection1.add(testPerson3);
 		
 		assertTrue(list.addAll(testCollection1));
 		assertTrue(list.containsAll(testCollection1));
 		
 		
-		Collection<Enrollment> testCollection2 = new ArrayList<>();
+		Collection<Person> testCollection2 = new ArrayList<>();
 		testCollection2.add(null);
 		
 		try {
@@ -224,7 +230,7 @@ public class cseLinkedListTest {
 		}
 		
 		testCollection2.clear();
-		testCollection2.add(testEnrollment1);
+		testCollection2.add(testPerson1);
 		
 		try {
 			list.addAll(testCollection2);
@@ -235,21 +241,21 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void addAllIndex() throws Exception {
-		Collection<Enrollment> testCollection1 = new ArrayList<>();
+		Collection<Person> testCollection1 = new ArrayList<>();
 		
-		testCollection1.add(testEnrollment3);
-		testCollection1.add(testEnrollment4);
+		testCollection1.add(testPerson3);
+		testCollection1.add(testPerson4);
 		
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
+		list.add(testPerson1);
+		list.add(testPerson2);
 		assertTrue(list.addAll(1, testCollection1));
 		
-		assertEquals(testEnrollment1, list.get(0));
-		assertEquals(testEnrollment3, list.get(1));
-		assertEquals(testEnrollment4, list.get(2));
-		assertEquals(testEnrollment2, list.get(3));
+		assertEquals(testPerson1, list.get(0));
+		assertEquals(testPerson3, list.get(1));
+		assertEquals(testPerson4, list.get(2));
+		assertEquals(testPerson2, list.get(3));
 		
-		Collection<Enrollment> testCollection2 = new ArrayList<>();
+		Collection<Person> testCollection2 = new ArrayList<>();
 		testCollection2.add(null);
 		
 		try {
@@ -265,7 +271,7 @@ public class cseLinkedListTest {
 		}
 		
 		testCollection2.clear();
-		testCollection2.add(testEnrollment1);
+		testCollection2.add(testPerson1);
 		
 		try {
 			list.addAll(1, testCollection2);
@@ -288,20 +294,20 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void removeAll() throws Exception {
-		Collection<Enrollment> testCollection1 = new ArrayList<>();
+		Collection<Person> testCollection1 = new ArrayList<>();
 		
-		testCollection1.add(testEnrollment1);
-		testCollection1.add(testEnrollment2);
-		testCollection1.add(testEnrollment3);
+		testCollection1.add(testPerson1);
+		testCollection1.add(testPerson2);
+		testCollection1.add(testPerson3);
 		
 		assertFalse("List should not have changed", list.removeAll(testCollection1));
 		
 		assertTrue(list.addAll(testCollection1));
 		assertTrue(list.containsAll(testCollection1));
 		
-		testCollection1.add(testEnrollment4); //Not removed because not in list, but removeAll should still return true
+		testCollection1.add(testPerson4); //Not removed because not in list, but removeAll should still return true
 		assertTrue(list.removeAll(testCollection1));
-		assertFalse(list.contains(testEnrollment1));
+		assertFalse(list.contains(testPerson1));
 		
 		try {
 			list.removeAll(null);
@@ -309,7 +315,7 @@ public class cseLinkedListTest {
 		} catch (NullPointerException shouldThrow) {
 		}
 		
-		Collection<Enrollment> testCollection2 = new ArrayList<>();
+		Collection<Person> testCollection2 = new ArrayList<>();
 		testCollection2.add(null);
 		try {
 			list.removeAll(testCollection2);
@@ -320,25 +326,25 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void retainAll() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
-		list.add(testEnrollment3);
-		list.add(testEnrollment4);
+		list.add(testPerson1);
+		list.add(testPerson2);
+		list.add(testPerson3);
+		list.add(testPerson4);
 		
-		Collection<Enrollment> testCollection1 = new ArrayList<>();
-		testCollection1.add(testEnrollment1);
-		testCollection1.add(testEnrollment2);
-		testCollection1.add(testEnrollment3);
-		testCollection1.add(testEnrollment4);
+		Collection<Person> testCollection1 = new ArrayList<>();
+		testCollection1.add(testPerson1);
+		testCollection1.add(testPerson2);
+		testCollection1.add(testPerson3);
+		testCollection1.add(testPerson4);
 		
 		assertFalse(list.retainAll(testCollection1));
 		
-		testCollection1.remove(testEnrollment2);
-		testCollection1.remove(testEnrollment4);
+		testCollection1.remove(testPerson2);
+		testCollection1.remove(testPerson4);
 		
 		assertTrue(list.retainAll(testCollection1));
-		assertEquals(testEnrollment1, list.get(0));
-		assertEquals(testEnrollment3, list.get(1));
+		assertEquals(testPerson1, list.get(0));
+		assertEquals(testPerson3, list.get(1));
 		assertEquals(2, list.size());
 		
 		try {
@@ -347,7 +353,7 @@ public class cseLinkedListTest {
 		} catch (NullPointerException shouldThrow) {
 		}
 		
-		Collection<Enrollment> testCollection2 = new ArrayList<>();
+		Collection<Person> testCollection2 = new ArrayList<>();
 		testCollection2.add(null);
 		try {
 			list.retainAll(testCollection2);
@@ -358,29 +364,29 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void clear() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
+		list.add(testPerson1);
+		list.add(testPerson2);
 		
 		list.clear();
 		assertEquals(0, list.size());
 		assertTrue(list.isEmpty());
-		assertFalse(list.contains(testEnrollment2));
+		assertFalse(list.contains(testPerson2));
 	}
 	
 	@Test
 	public void equals() throws Exception {
-		List<Enrollment> testList = new ArrayList<>();
+		List<Person> testList = new ArrayList<>();
 		
 	}
 	
 	@Test
 	public void get() throws Exception {
-		list.add(testEnrollment1);
-		assertEquals(testEnrollment1, list.get(0));
-		list.add(testEnrollment2);
-		assertEquals(testEnrollment2, list.get(1));
-		list.add(testEnrollment3);
-		assertEquals(testEnrollment3, list.get(2));
+		list.add(testPerson1);
+		assertEquals(testPerson1, list.get(0));
+		list.add(testPerson2);
+		assertEquals(testPerson2, list.get(1));
+		list.add(testPerson3);
+		assertEquals(testPerson3, list.get(2));
 		
 		try {
 			list.get(-1);
@@ -397,19 +403,19 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void set() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
-		list.add(testEnrollment3);
+		list.add(testPerson1);
+		list.add(testPerson2);
+		list.add(testPerson3);
 		
-		assertEquals(testEnrollment1, list.get(0));
-		assertEquals(testEnrollment2, list.get(1));
-		assertEquals(testEnrollment3, list.get(2));
+		assertEquals(testPerson1, list.get(0));
+		assertEquals(testPerson2, list.get(1));
+		assertEquals(testPerson3, list.get(2));
 		
-		assertEquals(testEnrollment2, list.set(1, testEnrollment4)); //Set returns element previously at that index
+		assertEquals(testPerson2, list.set(1, testPerson4)); //Set returns element previously at that index
 		
-		assertEquals(testEnrollment1, list.get(0));
-		assertEquals(testEnrollment4, list.get(1));
-		assertEquals(testEnrollment3, list.get(2));
+		assertEquals(testPerson1, list.get(0));
+		assertEquals(testPerson4, list.get(1));
+		assertEquals(testPerson3, list.get(2));
 		
 		try {
 			list.set(2, null);
@@ -418,19 +424,19 @@ public class cseLinkedListTest {
 		}
 		
 		try {
-			list.set(2, testEnrollment1);
+			list.set(2, testPerson1);
 			assertTrue("Did not throw exception for duplicate element", false);
 		} catch (IllegalArgumentException shouldThrow) {
 		}
 		
 		try {
-			list.set(-1, testEnrollment4);
+			list.set(-1, testPerson4);
 			assertTrue("Did not throw exception for negative index", false);
 		} catch (IndexOutOfBoundsException shouldThrow) {
 		}
 		
 		try {
-			list.set(99, testEnrollment4);
+			list.set(99, testPerson4);
 			assertTrue("Did not throw exception for out of bounds index", false);
 		} catch (IndexOutOfBoundsException shouldThrow) {
 		}
@@ -438,16 +444,16 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void addIndex() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
+		list.add(testPerson1);
+		list.add(testPerson2);
 		
-		list.add(1, testEnrollment3);
-		assertEquals(testEnrollment1, list.get(0));
-		assertEquals(testEnrollment3, list.get(1));
-		assertEquals(testEnrollment2, list.get(2));
+		list.add(1, testPerson3);
+		assertEquals(testPerson1, list.get(0));
+		assertEquals(testPerson3, list.get(1));
+		assertEquals(testPerson2, list.get(2));
 		
 		try {
-			list.add(1, testEnrollment1);
+			list.add(1, testPerson1);
 			assertTrue("Did not throw exception for duplicate element", false);
 		} catch (IllegalArgumentException shouldThrow) {
 		}
@@ -459,13 +465,13 @@ public class cseLinkedListTest {
 		}
 		
 		try {
-			list.add(-1, testEnrollment4);
+			list.add(-1, testPerson4);
 			assertTrue("Did not throw exception for negative index", false);
 		} catch (IndexOutOfBoundsException shouldThrow) {
 		}
 		
 		try {
-			list.add(99, testEnrollment4);
+			list.add(99, testPerson4);
 			assertTrue("Did not throw exception for out of bounds index", false);
 		} catch (IndexOutOfBoundsException shouldThrow) {
 		}
@@ -473,19 +479,19 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void removeIndex() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
-		list.add(testEnrollment3);
-		list.add(testEnrollment4);
+		list.add(testPerson1);
+		list.add(testPerson2);
+		list.add(testPerson3);
+		list.add(testPerson4);
 		
-		assertEquals(testEnrollment3, list.remove(2));
-		assertEquals(testEnrollment1, list.get(0));
-		assertEquals(testEnrollment2, list.get(1));
-		assertEquals(testEnrollment4, list.get(2));
+		assertEquals(testPerson3, list.remove(2));
+		assertEquals(testPerson1, list.get(0));
+		assertEquals(testPerson2, list.get(1));
+		assertEquals(testPerson4, list.get(2));
 		
-		assertEquals(testEnrollment1, list.remove(0));
-		assertEquals(testEnrollment2, list.get(0));
-		assertEquals(testEnrollment4, list.get(1));
+		assertEquals(testPerson1, list.remove(0));
+		assertEquals(testPerson2, list.get(0));
+		assertEquals(testPerson4, list.get(1));
 		
 		try {
 			list.remove(-1);
@@ -502,12 +508,12 @@ public class cseLinkedListTest {
 	
 	@Test
 	public void indexOf() throws Exception {
-		list.add(testEnrollment1);
-		list.add(testEnrollment2);
+		list.add(testPerson1);
+		list.add(testPerson2);
 		
-		assertEquals(0, list.indexOf(testEnrollment1));
-		assertEquals(1, list.indexOf(testEnrollment2));
-		assertEquals(-1, list.indexOf(testEnrollment3));
+		assertEquals(0, list.indexOf(testPerson1));
+		assertEquals(1, list.indexOf(testPerson2));
+		assertEquals(-1, list.indexOf(testPerson3));
 	}
 	
 	@Test
