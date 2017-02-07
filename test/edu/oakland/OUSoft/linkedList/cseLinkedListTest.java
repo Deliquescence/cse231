5c87f6e8cc7a32c842f2a8d63e841a7f79d9e07d
Josh Baird <jjbaird@oakland.edu>
2017-02-07 11:24:35 -0500

    Update test with a few different types

--- test/edu/oakland/OUSoft/linkedList/cseLinkedListTest.java
+++ test/edu/oakland/OUSoft/linkedList/cseLinkedListTest.java
@@ -16,6 +16,7 @@ import static org.junit.Assert.*;
 public class cseLinkedListTest {
 	
 	private cseLinkedList<Person> list;
+	private cseLinkedList<Student> list2;
 //	private ArrayList<Person> list;
 	
 	private Person testPerson1 = new Person("TestID1");
@@ -24,14 +25,15 @@ public class cseLinkedListTest {
 	private Person testPerson4 = new Person("TestID4");
 	
 	private Student testStudent1 = new Student("TestStudentID1");
-	private Student testStudent2 = new Student("TestStudentID2");
+	private Person testStudent2 = new Student("TestStudentID2");
 	
 	private Instructor testInstructor1 = new Instructor("TestInstructorID1");
-	private Instructor testInstructor2 = new Instructor("TestInstructorID2");
+	private Person testInstructor2 = new Instructor("TestInstructorID2");
 	
 	@Before
 	public void setUp() throws Exception {
 		list = new cseLinkedList<>();
+		list2 = new cseLinkedList<>();
 //		list = new ArrayList<>();
 	}
 	
@@ -59,7 +61,9 @@ public class cseLinkedListTest {
 	public void contains() throws Exception {
 		assertFalse(list.contains(testPerson1));
 		list.add(testPerson1);
+		list.add(testInstructor1);
 		assertTrue(list.contains(testPerson1));
+		assertTrue(list.contains(testInstructor1));
 		assertFalse(list.contains("Some object"));
 		assertFalse(list.contains(null));
 	}
@@ -68,12 +72,18 @@ public class cseLinkedListTest {
 	public void iterator() throws Exception {
 		list.add(testPerson1);
 		list.add(testPerson2);
+		list.add(testStudent1);
+		list.add(testInstructor1);
 		Iterator<Person> iterator = list.iterator();
 		
 		assertTrue(iterator.hasNext());
 		assertEquals("Iterator didn't give expected first element", testPerson1, iterator.next());
 		assertTrue(iterator.hasNext());
 		assertEquals("Iterator didn't give expected second element", testPerson2, iterator.next());
+		assertTrue(iterator.hasNext());
+		assertEquals("Iterator didn't give expected third element", testStudent1, iterator.next());
+		assertTrue(iterator.hasNext());
+		assertEquals("Iterator didn't give expected second element", testInstructor1, iterator.next());
 		assertFalse(iterator.hasNext());
 	}
 	
@@ -81,31 +91,33 @@ public class cseLinkedListTest {
 	public void toArray() throws Exception {
 		list.add(testPerson1);
 		list.add(testPerson2);
+		list.add(testStudent1);
 		
 		Object[] array = list.toArray();
 		assertEquals(testPerson1, array[0]);
 		assertEquals(testPerson2, array[1]);
+		assertEquals(testStudent1, array[2]);
 	}
 	
 	@Test
 	public void toArray1() throws Exception {
 		list.add(testPerson1);
-		list.add(testPerson2);
+		list.add(testInstructor1);
 		
 		Person[] array = list.toArray(new Person[0]);
 		assertEquals(testPerson1, array[0]);
-		assertEquals(testPerson2, array[1]);
+		assertEquals(testInstructor1, array[1]);
 		
 		Person[] array2 = new Person[2];
 		list.toArray(array2);
 		assertEquals(testPerson1, array2[0]);
-		assertEquals(testPerson2, array2[1]);
+		assertEquals(testInstructor1, array2[1]);
 		
 		Person[] array3 = new Person[3];
 		array3[2] = testPerson4;
 		list.toArray(array3);
 		assertEquals(testPerson1, array3[0]);
-		assertEquals(testPerson2, array3[1]);
+		assertEquals(testInstructor1, array3[1]);
 		assertNull(array3[2]);
 	}
 	
@@ -115,14 +127,26 @@ public class cseLinkedListTest {
 		assertTrue(list.contains(testPerson1));
 		assertEquals(testPerson1, list.get(0));
 		assertEquals(1, list.size());
+		
 		assertTrue(list.add(testPerson2));
 		assertTrue(list.contains(testPerson2));
 		assertEquals(testPerson2, list.get(1));
 		assertEquals(2, list.size());
 		
+		assertTrue(list.add(testStudent1));
+		assertTrue(list.contains(testStudent1));
+		assertEquals(testStudent1, list.get(2));
+		assertEquals(3, list.size());
+		
+		
+		assertTrue(list2.add(testStudent1));
+		assertTrue(list2.contains(testStudent1));
+		assertEquals(testStudent1, list2.get(0));
+		assertEquals(1, list2.size());
+		
 		//Do not allow duplicate
 		try {
-			list.add(testPerson1);
+			list.add(testStudent1);
 			assertTrue("Added a duplicate element", false);
 			
 		} catch (IllegalArgumentException ex) {
@@ -146,14 +170,14 @@ public class cseLinkedListTest {
 		assertFalse(list.contains(testPerson1));
 		
 		list.add(testPerson1);
-		list.add(testPerson2);
+		list.add(testStudent2);
 		list.add(testPerson3);
 		assertEquals(3, list.size());
 		
-		assertTrue(list.remove(testPerson2));
+		assertTrue(list.remove(testStudent2));
 		assertEquals(testPerson1, list.get(0));
 		assertEquals(testPerson3, list.get(1));
-		assertFalse(list.contains(testPerson2));
+		assertFalse(list.contains(testStudent2));
 		
 		try {
 			assertFalse("Should have returned false for null remove", list.remove(null));
@@ -164,7 +188,7 @@ public class cseLinkedListTest {
 	@Test
 	public void containsAll() throws Exception {
 		list.add(testPerson1);
-		list.add(testPerson2);
+		list.add(testStudent2);
 		list.add(testPerson3);
 		assertEquals(3, list.size());
 		
@@ -172,10 +196,10 @@ public class cseLinkedListTest {
 		Collection<Person> testCollection2 = new ArrayList<>();
 		Collection<Person> testCollection3 = new ArrayList<>();
 		
-		testCollection1.add(testPerson2);
+		testCollection1.add(testStudent2);
 		
 		testCollection2.add(testPerson1);
-		testCollection2.add(testPerson2);
+		testCollection2.add(testStudent2);
 		testCollection2.add(testPerson3);
 		
 		assertTrue(list.containsAll(testCollection1));
@@ -185,9 +209,10 @@ public class cseLinkedListTest {
 		list.remove(testPerson3);
 		assertFalse(list.containsAll(testCollection2));
 		
-		List<String> wrongType = new ArrayList<>();
+		List<Object> wrongType = new ArrayList<>();
 		wrongType.add("Checking for typing exceptions");
-		assertFalse(list.containsAll(wrongType));
+		wrongType.add(testInstructor2);
+		assertFalse(list2.containsAll(wrongType));
 		
 		try {
 			list.containsAll(null);
@@ -208,7 +233,7 @@ public class cseLinkedListTest {
 		Collection<Person> testCollection1 = new ArrayList<>();
 		
 		testCollection1.add(testPerson1);
-		testCollection1.add(testPerson2);
+		testCollection1.add(testStudent2);
 		testCollection1.add(testPerson3);
 		
 		assertTrue(list.addAll(testCollection1));
@@ -245,7 +270,7 @@ public class cseLinkedListTest {
 		Collection<Person> testCollection1 = new ArrayList<>();
 		
 		testCollection1.add(testPerson3);
-		testCollection1.add(testPerson4);
+		testCollection1.add(testInstructor2);
 		
 		list.add(testPerson1);
 		list.add(testPerson2);
@@ -253,7 +278,7 @@ public class cseLinkedListTest {
 		
 		assertEquals(testPerson1, list.get(0));
 		assertEquals(testPerson3, list.get(1));
-		assertEquals(testPerson4, list.get(2));
+		assertEquals(testInstructor2, list.get(2));
 		assertEquals(testPerson2, list.get(3));
 		
 		Collection<Person> testCollection2 = new ArrayList<>();
@@ -389,6 +414,11 @@ public class cseLinkedListTest {
 		list.add(testPerson3);
 		assertEquals(testPerson3, list.get(2));
 		
+		list.add(testInstructor1);
+		assertEquals(testInstructor1, list.get(3));
+		list.add(testInstructor2);
+		assertEquals(testInstructor2, list.get(4));
+		
 		try {
 			list.get(-1);
 			assertTrue("Did not throw exception for negative index", false);
@@ -405,17 +435,17 @@ public class cseLinkedListTest {
 	@Test
 	public void set() throws Exception {
 		list.add(testPerson1);
-		list.add(testPerson2);
+		list.add(testInstructor2);
 		list.add(testPerson3);
 		
 		assertEquals(testPerson1, list.get(0));
-		assertEquals(testPerson2, list.get(1));
+		assertEquals(testInstructor2, list.get(1));
 		assertEquals(testPerson3, list.get(2));
 		
-		assertEquals(testPerson2, list.set(1, testPerson4)); //Set returns element previously at that index
+		assertEquals(testInstructor2, list.set(1, testStudent1)); //Set returns element previously at that index
 		
 		assertEquals(testPerson1, list.get(0));
-		assertEquals(testPerson4, list.get(1));
+		assertEquals(testStudent1, list.get(1));
 		assertEquals(testPerson3, list.get(2));
 		
 		try {
@@ -446,12 +476,12 @@ public class cseLinkedListTest {
 	@Test
 	public void addIndex() throws Exception {
 		list.add(testPerson1);
-		list.add(testPerson2);
+		list.add(testInstructor2);
 		
-		list.add(1, testPerson3);
+		list.add(1, testStudent1);
 		assertEquals(testPerson1, list.get(0));
-		assertEquals(testPerson3, list.get(1));
-		assertEquals(testPerson2, list.get(2));
+		assertEquals(testStudent1, list.get(1));
+		assertEquals(testInstructor2, list.get(2));
 		
 		try {
 			list.add(1, testPerson1);
@@ -481,17 +511,17 @@ public class cseLinkedListTest {
 	@Test
 	public void removeIndex() throws Exception {
 		list.add(testPerson1);
-		list.add(testPerson2);
+		list.add(testInstructor2);
 		list.add(testPerson3);
 		list.add(testPerson4);
 		
 		assertEquals(testPerson3, list.remove(2));
 		assertEquals(testPerson1, list.get(0));
-		assertEquals(testPerson2, list.get(1));
+		assertEquals(testInstructor2, list.get(1));
 		assertEquals(testPerson4, list.get(2));
 		
 		assertEquals(testPerson1, list.remove(0));
-		assertEquals(testPerson2, list.get(0));
+		assertEquals(testInstructor2, list.get(0));
 		assertEquals(testPerson4, list.get(1));
 		
 		try {
@@ -510,10 +540,10 @@ public class cseLinkedListTest {
 	@Test
 	public void indexOf() throws Exception {
 		list.add(testPerson1);
-		list.add(testPerson2);
+		list.add(testInstructor2);
 		
 		assertEquals(0, list.indexOf(testPerson1));
-		assertEquals(1, list.indexOf(testPerson2));
+		assertEquals(1, list.indexOf(testInstructor2));
 		assertEquals(-1, list.indexOf(testPerson3));
 	}
 	
