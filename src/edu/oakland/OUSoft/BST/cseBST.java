8b5801d42457bbb24b0f15b1b4eed0740f96b32d
Josh Baird <jjbaird@oakland.edu>
2017-03-28 12:25:12 -0400

    Implement contains

--- src/edu/oakland/OUSoft/BST/cseBST.java
+++ src/edu/oakland/OUSoft/BST/cseBST.java
@@ -1,11 +1,23 @@
 package edu.oakland.OUSoft.BST;
 
-import java.util.Iterator;
-
 public class cseBST<E extends Comparable<E>> {
 	
+	private Node<E> root;
 	
-	public boolean contains(Object o) {
+	public boolean contains(E o) {
+		return containsRecurse(root, o);
+	}
+	
+	private boolean containsRecurse(Node<E> node, E e) {
+		if (node.element.equals(e)) {
+			return true;
+		} else {
+			if (e.compareTo(node.element) <= 0) {
+				return containsRecurse(node.left, e);
+			} else {
+				return containsRecurse(node.right, e);
+			}
+		}
 	}
 	
 	public boolean add(E e) {
@@ -21,12 +33,17 @@ public class cseBST<E extends Comparable<E>> {
 	}
 	
 	public void clear() {
+		root = new Node<>(null);
 	}
 	
-	private class node<E> {
-		E root;
-		node<E> left;
-		node<E> right;
+	private class Node<E> {
+		E element;
+		Node<E> left;
+		Node<E> right;
+		
+		public Node(E e) {
+			this.element = e;
+		}
 	}
 	
 }
