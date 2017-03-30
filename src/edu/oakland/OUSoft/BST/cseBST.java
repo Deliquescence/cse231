48981f4e2c03935f0b4015c2f0fa8a0f3f940b28
Josh Baird <jjbaird@oakland.edu>
2017-03-30 11:25:34 -0400

    Have cseBST implement Collection

--- src/edu/oakland/OUSoft/BST/cseBST.java
+++ src/edu/oakland/OUSoft/BST/cseBST.java
@@ -1,6 +1,9 @@
 package edu.oakland.OUSoft.BST;
 
-public class cseBST<E extends Comparable<E>> {
+import java.util.Collection;
+import java.util.Iterator;
+
+public class cseBST<E extends Comparable<E>> implements Collection<E> {
 	
 	private Node<E> root;
 	
@@ -39,8 +42,15 @@ public class cseBST<E extends Comparable<E>> {
 		return node;
 	}
 	
-	public void remove(E e) {
-		root = removeRecurse(root, e);
+	/**
+	 * Remove an object from the tree.
+	 *
+	 * @param o the object to remove
+	 */
+	public boolean remove(Object o) {
+		root = removeRecurse(root, (E) o);
+		
+		return true; //Not implemented yet
 	}
 	
 	private Node<E> removeRecurse(Node<E> node, E e) {
@@ -90,6 +100,215 @@ public class cseBST<E extends Comparable<E>> {
 	
 	public boolean isEmpty() {
 		return root == null;
+	}
+	
+	/**
+	 * Returns <tt>true</tt> if this collection contains the specified element.
+	 * More formally, returns <tt>true</tt> if and only if this collection
+	 * contains at least one element <tt>e</tt> such that
+	 * <tt>(o==null&nbsp;?&nbsp;e==null&nbsp;:&nbsp;o.equals(e))</tt>.
+	 *
+	 * @param o element whose presence in this collection is to be tested
+	 * @return <tt>true</tt> if this collection contains the specified
+	 * element
+	 * @throws ClassCastException   if the type of the specified element
+	 *                              is incompatible with this collection
+	 *                              (<a href="#optional-restrictions">optional</a>)
+	 * @throws NullPointerException if the specified element is null and this
+	 *                              collection does not permit null elements
+	 *                              (<a href="#optional-restrictions">optional</a>)
+	 */
+	@Override
+	public boolean contains(Object o) {
+		return contains((E) o);
+	}
+	
+	/**
+	 * Returns an iterator over the elements in this collection.  There are no
+	 * guarantees concerning the order in which the elements are returned
+	 * (unless this collection is an instance of some class that provides a
+	 * guarantee).
+	 *
+	 * @return an <tt>Iterator</tt> over the elements in this collection
+	 */
+	@Override
+	public Iterator<E> iterator() {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+	/**
+	 * Returns an array containing all of the elements in this collection.
+	 * If this collection makes any guarantees as to what order its elements
+	 * are returned by its iterator, this method must return the elements in
+	 * the same order.
+	 * <p>
+	 * <p>The returned array will be "safe" in that no references to it are
+	 * maintained by this collection.  (In other words, this method must
+	 * allocate a new array even if this collection is backed by an array).
+	 * The caller is thus free to modify the returned array.
+	 * <p>
+	 * <p>This method acts as bridge between array-based and collection-based
+	 * APIs.
+	 *
+	 * @return an array containing all of the elements in this collection
+	 */
+	@Override
+	public Object[] toArray() {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+	/**
+	 * Returns an array containing all of the elements in this collection;
+	 * the runtime type of the returned array is that of the specified array.
+	 * If the collection fits in the specified array, it is returned therein.
+	 * Otherwise, a new array is allocated with the runtime type of the
+	 * specified array and the size of this collection.
+	 * <p>
+	 * <p>If this collection fits in the specified array with room to spare
+	 * (i.e., the array has more elements than this collection), the element
+	 * in the array immediately following the end of the collection is set to
+	 * <tt>null</tt>.  (This is useful in determining the length of this
+	 * collection <i>only</i> if the caller knows that this collection does
+	 * not contain any <tt>null</tt> elements.)
+	 * <p>
+	 * <p>If this collection makes any guarantees as to what order its elements
+	 * are returned by its iterator, this method must return the elements in
+	 * the same order.
+	 * <p>
+	 * <p>Like the {@link #toArray()} method, this method acts as bridge between
+	 * array-based and collection-based APIs.  Further, this method allows
+	 * precise control over the runtime type of the output array, and may,
+	 * under certain circumstances, be used to save allocation costs.
+	 * <p>
+	 * <p>Suppose <tt>x</tt> is a collection known to contain only strings.
+	 * The following code can be used to dump the collection into a newly
+	 * allocated array of <tt>String</tt>:
+	 * <p>
+	 * <pre>
+	 *     String[] y = x.toArray(new String[0]);</pre>
+	 * <p>
+	 * Note that <tt>toArray(new Object[0])</tt> is identical in function to
+	 * <tt>toArray()</tt>.
+	 *
+	 * @param a the array into which the elements of this collection are to be
+	 *          stored, if it is big enough; otherwise, a new array of the same
+	 *          runtime type is allocated for this purpose.
+	 * @return an array containing all of the elements in this collection
+	 * @throws ArrayStoreException  if the runtime type of the specified array
+	 *                              is not a supertype of the runtime type of every element in
+	 *                              this collection
+	 * @throws NullPointerException if the specified array is null
+	 */
+	@Override
+	public <T> T[] toArray(T[] a) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+	/**
+	 * Returns <tt>true</tt> if this collection contains all of the elements
+	 * in the specified collection.
+	 *
+	 * @param c collection to be checked for containment in this collection
+	 * @return <tt>true</tt> if this collection contains all of the elements
+	 * in the specified collection
+	 * @throws ClassCastException   if the types of one or more elements
+	 *                              in the specified collection are incompatible with this
+	 *                              collection
+	 *                              (<a href="#optional-restrictions">optional</a>)
+	 * @throws NullPointerException if the specified collection contains one
+	 *                              or more null elements and this collection does not permit null
+	 *                              elements
+	 *                              (<a href="#optional-restrictions">optional</a>),
+	 *                              or if the specified collection is null.
+	 * @see #contains(Object)
+	 */
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+	/**
+	 * Adds all of the elements in the specified collection to this collection
+	 * (optional operation).  The behavior of this operation is undefined if
+	 * the specified collection is modified while the operation is in progress.
+	 * (This implies that the behavior of this call is undefined if the
+	 * specified collection is this collection, and this collection is
+	 * nonempty.)
+	 *
+	 * @param c collection containing elements to be added to this collection
+	 * @return <tt>true</tt> if this collection changed as a result of the call
+	 * @throws UnsupportedOperationException if the <tt>addAll</tt> operation
+	 *                                       is not supported by this collection
+	 * @throws ClassCastException            if the class of an element of the specified
+	 *                                       collection prevents it from being added to this collection
+	 * @throws NullPointerException          if the specified collection contains a
+	 *                                       null element and this collection does not permit null elements,
+	 *                                       or if the specified collection is null
+	 * @throws IllegalArgumentException      if some property of an element of the
+	 *                                       specified collection prevents it from being added to this
+	 *                                       collection
+	 * @throws IllegalStateException         if not all the elements can be added at
+	 *                                       this time due to insertion restrictions
+	 * @see #add(Object)
+	 */
+	@Override
+	public boolean addAll(Collection<? extends E> c) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+	/**
+	 * Removes all of this collection's elements that are also contained in the
+	 * specified collection (optional operation).  After this call returns,
+	 * this collection will contain no elements in common with the specified
+	 * collection.
+	 *
+	 * @param c collection containing elements to be removed from this collection
+	 * @return <tt>true</tt> if this collection changed as a result of the
+	 * call
+	 * @throws UnsupportedOperationException if the <tt>removeAll</tt> method
+	 *                                       is not supported by this collection
+	 * @throws ClassCastException            if the types of one or more elements
+	 *                                       in this collection are incompatible with the specified
+	 *                                       collection
+	 *                                       (<a href="#optional-restrictions">optional</a>)
+	 * @throws NullPointerException          if this collection contains one or more
+	 *                                       null elements and the specified collection does not support
+	 *                                       null elements
+	 *                                       (<a href="#optional-restrictions">optional</a>),
+	 *                                       or if the specified collection is null
+	 * @see #remove(Object)
+	 * @see #contains(Object)
+	 */
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+	
+	/**
+	 * Retains only the elements in this collection that are contained in the
+	 * specified collection (optional operation).  In other words, removes from
+	 * this collection all of its elements that are not contained in the
+	 * specified collection.
+	 *
+	 * @param c collection containing elements to be retained in this collection
+	 * @return <tt>true</tt> if this collection changed as a result of the call
+	 * @throws UnsupportedOperationException if the <tt>retainAll</tt> operation
+	 *                                       is not supported by this collection
+	 * @throws ClassCastException            if the types of one or more elements
+	 *                                       in this collection are incompatible with the specified
+	 *                                       collection
+	 *                                       (<a href="#optional-restrictions">optional</a>)
+	 * @throws NullPointerException          if this collection contains one or more
+	 *                                       null elements and the specified collection does not permit null
+	 *                                       elements
+	 *                                       (<a href="#optional-restrictions">optional</a>),
+	 *                                       or if the specified collection is null
+	 * @see #remove(Object)
+	 * @see #contains(Object)
+	 */
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException("Not implemented yet.");
 	}
 	
 	public void clear() {
